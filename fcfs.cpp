76bce9e25e70b7501a6908259a945487ca9eaa40
Shivendra Saurav <shivendrasaurav@gmail.com>
2019-11-20 15:25:45 +0530

    FCFS Scheduling

--- fcfs.cpp
+++ fcfs.cpp
@@ -1,5 +1,28 @@
 #include<stdio.h>
 
 int main(){
-	
+	int n,i,j,at;
+	printf("\nEnter Number Of Processes: ");
+	scanf("%d",&n);
+	printf("\nEnter Arrival Time In Seconds : ");
+	scanf("%d", &at);
+	int dur[n], bt[n], wt[n], tat[n];
+	printf("\nEnter Burst Time Array For Processes: \n");
+	for(i=0;i<n;i++)
+		scanf("%d", &bt[i]);
+	wt[0]=at;
+	for(i=1;i<n;i++){
+		wt[i] = bt[i-1] + wt[i-1];
+		printf("%d\t", wt[i]);
+	}
+	for(i=0;i<n;i++){
+		tat[i] = wt[i] + bt[i];		
+		printf("%d\t", tat[i]);
+	}
+	float avgwt, avgtat, totwt=0, totat=0;
+	for(i=0;i<n;i++){
+		totwt = totwt + wt[i];
+		totat = totwt + tat[i];
+	}
+	printf("\nAverage Waiting Time: %f \n\nAverage Turnaround Time: %f", totwt/n, totat/n);
 }
