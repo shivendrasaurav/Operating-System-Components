2649ab9a21f08722f4c1ef8befb2b75812a8df73
Shivendra Saurav <shivendrasaurav@gmail.com>
2019-11-20 10:44:41 +0530

    Continuous File Allocation

---
+++ cont_file_alloc.cpp
@@ -0,0 +1,37 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+	int n,i,j,fs,ch,sb;
+	printf("\nEnter Memory Space: ");
+	scanf("%d",&n);
+	int mem[n]={0};
+	while(1){
+	printf("\n1. Insert More Memory \n2. Exit \nEnter Your Choice: ");
+	scanf("%d",&ch);
+	if(ch==1){
+		printf("\nEnter file size in MB: ");
+		scanf("%d",&fs);
+		printf("\nEnter Starting Block: ");
+		scanf("%d",&sb);
+		int eb=sb+fs+1;
+		for(j=sb;j<eb;j++){
+			if(mem[j]==1){
+				printf("block %d is already full, skipping this block", j);
+				eb++;
+			}
+			else if(mem[j]==0){
+				mem[j]=1;
+				printf("\nSpace Allocated at block : %d", j);
+			}
+		}
+		printf("\n\nStatus of memory: \n");
+		for(i=0;i<n;i++)
+			printf("%d\t", mem[i]);
+	}
+	else if(ch==2)
+		exit(0);
+	else
+		continue;
+	}
+}
